aa6a020fe9a4a6f33b2607f1b33d77541242d28f
Greg Thimmes <gregthimmes@Gregs-MacBook-Pro.local>
2019-12-28 22:05:55 -0500

    update for profile page

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -8,12 +8,16 @@ import Tooltip from "@material-ui/core/Tooltip";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 // @material-ui/icons
-import Camera from "@material-ui/icons/Camera";
+import Face from "@material-ui/icons/Face";
+import Store from "@material-ui/icons/Store";
+import OnDemendVideo from "@material-ui/icons/OndemandVideo";
 import Palette from "@material-ui/icons/Palette";
 import People from "@material-ui/icons/People";
 import Add from "@material-ui/icons/Add";
 import Favorite from "@material-ui/icons/Favorite";
 import ThumbUp from "@material-ui/icons/ThumbUp";
+import Donate from "@material-ui/icons/Money";
+import Message from "@material-ui/icons/Message";
 
 // core components
 import Header from "components/Header/Header.js";
@@ -22,8 +26,9 @@ import GridItem from "components/Grid/GridItem.js";
 import HeaderLinks from "components/Header/HeaderLinks.js";
 import NavPills from "components/NavPills/NavPills.js";
 import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
 import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
 import Badge from "components/Badge/Badge.js";
 import Muted from "components/Typography/Muted.js";
 import Parallax from "components/Parallax/Parallax.js";
@@ -75,6 +80,8 @@ export default function ProfilePage({ ...rest }) {
     {
       setProfileLikeColor('github');
     }
+
+    return false;
     
   }
 
@@ -113,35 +120,10 @@ export default function ProfilePage({ ...rest }) {
                 <i className="material-icons md-48 orange600">star</i>
                 <i className="material-icons md-48 orange600">star</i>
                 <i className="material-icons md-48 orange600">star</i>
-                <i className="material-icons md-48 orange600">star</i>
-
-
+                <i className="material-icons md-48 orange600">star_half</i>
+                <br/>239 Reviews
                   <h3 className={classes.title}>Christian Louboutin</h3>
                   <h6>INFLUENCER</h6>
-                  <Button
-                    justIcon
-                    simple
-                    color="instagram"
-                    className={classes.margin5}
-                  >
-                    <i className={classes.socials + " fab fa-instagram"} />
-                  </Button>
-                  <Button
-                    justIcon
-                    simple
-                    color="twitter"
-                    className={classes.margin5}
-                  >
-                    <i className={classes.socials + " fab fa-twitter"} />
-                  </Button>
-                  <Button
-                    justIcon
-                    simple
-                    color="pinterest"
-                    className={classes.margin5}
-                  >
-                    <i className={classes.socials + " fab fa-pinterest"} />
-                  </Button>
                 </div>
               </div>
               <div className={classes.follow}>
@@ -176,23 +158,48 @@ export default function ProfilePage({ ...rest }) {
                     <ThumbUp className={classes.icon} />
                   </Button>
                 </Tooltip>
+                <Tooltip
+                  id="tooltip-top"
+                  title="Message"
+                  placement="top"
+                  classes={{ tooltip: classes.tooltip }}
+                >
+                  <Button
+                    justIcon
+                    round
+                    color="github"
+                    className={classes.followButton}
+                  >
+                    <Message className={classes.icon} />
+                  </Button>
+                </Tooltip>
+                <Tooltip
+                  id="tooltip-top"
+                  title="Donate"
+                  placement="top"
+                  classes={{ tooltip: classes.tooltip }}
+                >
+                  <Button
+                    justIcon
+                    round
+                    color="github"
+                    className={classes.followButton}
+                  >
+                    <Donate className={classes.icon} />
+                  </Button>
+                </Tooltip>
                 
               </div>
             </GridItem>
           </GridContainer>
-          <div className={classNames(classes.description, classes.textCenter)}>
-            <p>
-              A “Me in 30 Seconds” statement is a simple way to present to someone else a balanced understanding of who you are. It piques the interest of a listener who invites you to “Tell me a little about yourself,” and it provides a brief and compelling answer to the question “Why should I buy from you?” {" "}
-            </p>
-          </div>
           <div className={classes.profileTabs}>
             <NavPills
               alignCenter
               color="info"
               tabs={[
                 {
-                  tabButton: "Videos",
-                  tabIcon: Palette,
+                  tabButton: "About Me",
+                  tabIcon: Face,
                   tabContent: (
                     <GridContainer>
                       <GridItem
@@ -201,89 +208,14 @@ export default function ProfilePage({ ...rest }) {
                         md={7}
                         className={classes.gridItem}
                       >
-                        <h4 className={classes.title}>Latest Videos</h4>
                         <GridContainer className={classes.collections}>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card
-                              background
-                              style={{
-                                backgroundImage: "url(" + streetfighter + ")"
-                              }}
-                            >
-                              <CardBody background className={classes.cardBody}>
-                                <Badge
-                                  color="warning"
-                                  className={classes.badge}
-                                >
-                                  Electronics
-                                </Badge>
-                                <a href="#/video-page">
-                                  <h2 className={classes.cardTitleWhite}>
-                                    Arcade 1Up Full Review Street Fighter Edition - Is it Worth $300?
-                                  </h2>
-                                </a>
-                              </CardBody>
-                            </Card>
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card
-                              background
-                              style={{
-                                backgroundImage: "url(" + rampage + ")"
-                              }}
-                            >
-                              <CardBody background className={classes.cardBody}>
-                                <Badge color="warning" className={classes.badge}>
-                                  Electronics
-                                </Badge>
-                                <a href="#/video-page">
-                                  <h2 className={classes.cardTitleWhite}>
-                                    Arcade1Up Rampage Arcade Cabinet Review
-                                  </h2>
-                                </a>
-                              </CardBody>
-                            </Card>
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card
-                              background
-                              style={{
-                                backgroundImage: "url(" + nike2019 + ")"
-                              }}
-                            >
-                              <CardBody background className={classes.cardBody}>
-                                <Badge color="primary" className={classes.badge}>
-                                  Footwear
-                                </Badge>
-                                <a href="#/video-page">
-                                  <h2 className={classes.cardTitleWhite}>
-                                    Nike Downshifter
-                                  </h2>
-                                </a>
-                              </CardBody>
-                            </Card>
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Card
-                              background
-                              style={{
-                                backgroundImage: "url(" + avengerstoy + ")"
-                              }}
-                            >
-                              <CardBody background className={classes.cardBody}>
-                                <Badge
-                                  color="rose"
-                                  className={classes.badge}
-                                >
-                                  Toys
-                                </Badge>
-                                <a href="#/video-page">
-                                  <h2 className={classes.cardTitleWhite}>
-                                    Endgame Power Gauntlet
-                                  </h2>
-                                </a>
-                              </CardBody>
-                            </Card>
+                          <GridItem xs={12} sm={12} md={12}>
+                          <div className={classNames(classes.description)}>
+                            <p>
+                              A “Me in 30 Seconds” statement is a simple way to present to someone else a balanced understanding of who you are. It piques the interest of a listener who invites you to “Tell me a little about yourself,” and it provides a brief and compelling answer to the question “Why should I buy from you?” {" "}
+                            </p>
+                          </div>
+                           <iframe width="100%" height="315" src="https://www.youtube.com/embed/ahZFCF--uRY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                           </GridItem>
                         </GridContainer>
                       </GridItem>
@@ -293,6 +225,30 @@ export default function ProfilePage({ ...rest }) {
                         md={2}
                         className={classes.gridItem}
                       >
+                        <Button
+                        justIcon
+                        simple
+                        color="instagram"
+                        className={classes.margin5}
+                      >
+                        <i className={classes.socials + " fab fa-instagram"} />
+                      </Button>
+                      <Button
+                        justIcon
+                        simple
+                        color="twitter"
+                        className={classes.margin5}
+                      >
+                        <i className={classes.socials + " fab fa-twitter"} />
+                      </Button>
+                      <Button
+                        justIcon
+                        simple
+                        color="pinterest"
+                        className={classes.margin5}
+                      >
+                        <i className={classes.socials + " fab fa-pinterest"} />
+                      </Button>
                         <h4 className={classes.title}>Stats</h4>
                         <ul className={classes.listUnstyled}>
                           <li>
@@ -315,6 +271,38 @@ export default function ProfilePage({ ...rest }) {
                         <Badge color="rose">Toys</Badge>
                       </GridItem>
                     </GridContainer>
+                  )
+                },
+                {
+                  tabButton: "Videos",
+                  tabIcon: OnDemendVideo,
+                  tabContent: (
+                    <GridContainer>
+              <GridItem sm={6} md={3}>
+                <Card product>
+                  <CardHeader image>
+                    <a href="#pablo">
+                      <img src={rampage} alt="cardProduct" />
+                    </a>
+                  </CardHeader>
+                  <CardBody>
+                    <h6
+                      className={classNames(
+                        classes.cardCategory,
+                        classes.textRose
+                      )}
+                    >
+                      Electronics
+                    </h6>
+                    <h4 className={classes.cardTitle}>Arcade 1UP Rampage</h4>
+                    <div className={classes.cardDescription}>
+                    Climb buildings, jump from one to another, and smash them down while searching for food.  Rampage is being brought back to life with this officially licensed home arcade cabinet.
+                    </div>
+                  </CardBody>
+                </Card>
+              </GridItem>
+      
+            </GridContainer>
                   )
                 },
                 {
@@ -488,33 +476,26 @@ export default function ProfilePage({ ...rest }) {
                   )
                 },
                 {
-                  tabButton: "Media",
-                  tabIcon: Camera,
+                  tabButton: "Products",
+                  tabIcon: Store,
                   tabContent: (
-                    <GridContainer justify="center">
-                      <GridItem xs={12} sm={12} md={3}>
+                    <GridContainer>
+            
+                      <GridItem xs={4} sm={4} md={4}>
                         <img
                           alt="..."
                           src={streetfighter}
-                          className={navImageClasses}
-                        />
-                        <img
-                          alt="..."
-                          src={rampage}
                           className={navImageClasses}
                         />
                       </GridItem>
-                      <GridItem xs={12} sm={12} md={3}>
-                        <img
-                          alt="..."
-                          src={streetfighter}
-                          className={navImageClasses}
-                        />
+                      <GridItem xs={4} sm={4} md={4}>
                         <img
                           alt="..."
                           src={nike2019}
                           className={navImageClasses}
                         />
+                         </GridItem>
+                        <GridItem xs={4} sm={4} md={4}>
                         <img
                           alt="..."
                           src={cynthiaDelRio}
@@ -527,7 +508,7 @@ export default function ProfilePage({ ...rest }) {
               ]}
             />
           </div>
-          <Clearfix />
+         
         </div>
       </div>
     </div>
